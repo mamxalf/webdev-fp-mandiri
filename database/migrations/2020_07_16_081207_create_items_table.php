f57437267ccf96c551d94a11c3bc68be15682375
mamxalf <hammamxalf@gmail.com>
2020-07-16 15:21:56 +0700

    second commit

---
+++ database/migrations/2020_07_16_081207_create_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('id_category')->unsigned();
+            $table->string('name');
+            $table->string('code')->unique();
+            $table->string('price');
+            $table->timestamps();
+
+            $table->foreign('id_category')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
